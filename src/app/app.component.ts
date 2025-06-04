61311f1e38f4ac8603a87df7f47c205bc1806cf8
Mildred Naab <mwinkomnaab@gmail.com>
2025-06-04 21:14:43 +0000

    Add invoice list layout and filter functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { InvoiceListComponent } from './components/invoice-list/invoice-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, InvoiceListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/components/invoice-list/invoice-list.component.ts
+++ src/app/components/invoice-list/invoice-list.component.ts
@@ -1,11 +1,48 @@
 import { Component } from '@angular/core';
+import { StatusBadgeComponent } from '../status-badge/status-badge.component';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+
+type Status = 'paid' | 'pending' | 'draft';
 
 @Component({
   selector: 'app-invoice-list',
-  imports: [],
+  imports: [CommonModule, RouterModule, StatusBadgeComponent, FormsModule],
   templateUrl: './invoice-list.component.html',
-  styleUrl: './invoice-list.component.scss'
+  styleUrls: ['./invoice-list.component.scss'],
 })
-export class InvoiceListComponent {
 
+export class InvoiceListComponent {
+  selectedStatus: string = '';
+
+  get filteredInvoices() {
+    return this.selectedStatus
+      ? this.invoices.filter(inv => inv.status === this.selectedStatus)
+      : this.invoices;
+  }
+
+  invoices: {
+    id: string;
+    clientName: string;
+    paymentDue: string;
+    total: number;
+    status: Status;
+  }[] = [
+    {
+      id: 'RT3080',
+      clientName: 'Mildred Naab',
+      paymentDue: '2025-06-15',
+      total: 375.0,
+      status: 'pending',
+    },
+    {
+      id: 'XM9141',
+      clientName: 'Ella Aye',
+      paymentDue: '2025-06-10',
+      total: 2250.0,
+      status: 'paid',
+    },
+  ];
 }
